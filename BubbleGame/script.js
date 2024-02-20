c82695589c29578c6a4450f13381f77c74a51aad
NARESH KUMAR <142076994+nareshkumarhp14s@users.noreply.github.com>
2024-02-20 13:56:48 +0530

    Add files via upload

---
+++ BubbleGame/script.js
@@ -0,0 +1,53 @@
+var Timer = 60;
+var score = 0;
+var hitrn = 0;
+function makeBubble() {
+    var Store = "";
+    for (var i = 1; i <= 140; i++) {
+        var randomNum = Math.floor(Math.random() * 10);
+        Store += `<div id="bubble">${randomNum}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = Store;
+}
+function getNewhit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#Hitval").textContent = hitrn;
+}
+function runTimer() {
+    var Timerint = setInterval(function () {
+        if (Timer > 0) {
+            Timer--;
+            document.querySelector("#timer").textContent = Timer;
+
+        }
+        else {
+            clearInterval(Timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game over = ${score} </h1>`;
+
+        }
+
+    }, 1000);
+
+}
+function increasseScore() {
+    score += 10;
+    document.querySelector("#Scoreval").textContent = score;
+}
+function reloadPage() {
+    location.reload();
+}
+document.querySelector("#pbtm").addEventListener("click", function (details) {
+    var clickednum = Number(details.target.textContent);
+    if (clickednum == hitrn) {
+        increasseScore();
+        makeBubble();
+        getNewhit();
+    }
+});
+document.querySelector("#reloadButton").addEventListener("click", function () {
+    reloadPage();
+});
+getNewhit();
+runTimer();
+makeBubble();
+
